18f078b8d750c2ee66e8fe71e821e8ef147620df
chuanbao.yang <chuanbao.yang@dajie-inc.com>
2014-07-24 19:16:34 +0800

    grunt  imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,52 @@
                       'css/base-min.css': ['src/css/reset.css', 'src/css/base.css']
                   }
               }
+          },
+          imagemin: {                          // Task
+              /*static: {                          // Target
+                  options: {                       // Target options
+                      optimizationLevel: 3,
+                      use: [mozjpeg()]
+                  },
+                  files: {                         // Dictionary of files
+                      'dist/img.png': 'src/img.png', // 'destination': 'source'
+                      'dist/img.jpg': 'src/img.jpg',
+                      'dist/img.gif': 'src/img.gif'
+                  }
+              },*/
+              dynamic: {                         // Another target
+                  files: [{
+                      expand: true,                  // Enable dynamic expansion
+                      cwd: 'src/i',                   // Src matches are relative to this path
+                      src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                      dest: 'i/'                  // Destination path prefix
+                  }]
+              }
+          },
+
+          watch: {
+              css: {
+                  files: ['src/css/*.css'],
+                  tasks: ['concat'],
+                  options: {
+                      spawn: false
+                  }
+              },
+              js: {
+                  files: ['src/js/*.js'],
+                  tasks: ['uglify'],
+                  options: {
+                      spawn: false
+                  }
+              },
+              image: {
+                  files: ['src/i/*.{png,jpg,gif}'],
+                  tasks: ['imagemin'],
+                  options: {
+                      spawn: false
+                  }
+              }
+
           }
   });
 
@@ -37,7 +83,9 @@
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
-  grunt.registerTask('default', ['uglify','concat']);
+  grunt.registerTask('default', ['uglify','concat','imagemin','watch']);
 
 };
